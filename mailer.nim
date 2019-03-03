4800c5b507ba92aab315b6163c4373b02080f171
ThomasTJdev <ttjarloev@gmail.com>
2019-03-03 12:44:17 +0100

    Update to support NimWC 5.0.0

--- mailer.nim
+++ mailer.nim
@@ -10,19 +10,20 @@
 import
   asyncdispatch,
   asyncnet,
-  db_sqlite,
+  datetime2human,
   json,
-  logging
+  logging,
   os,
   parsecfg,
   strutils,
 
+when defined(postgres): import db_postgres
+else:                   import db_sqlite
 
 from times import epochTime
 import jester
 import ../../nimwcpkg/resources/email/email_connection
 import ../../nimwcpkg/resources/session/user_data
-import ../../nimwcpkg/resources/utils/dates
 import ../../nimwcpkg/resources/utils/logging_nimwc
 import ../../nimwcpkg/resources/utils/plugins
 
@@ -45,55 +46,51 @@ include "html.tmpl"
 proc mailerAddMailevent*(c: var TData, db: DbConn): string =
   ## Add a mail event
 
-  if c.loggedIn:
-    let name        = c.req.params["name"]
-    let status      = c.req.params["status"]
-    let description = c.req.params["mailtext"]
-    let timezone    = parseInt(c.req.params["timezone"].substr(1,2)) * 60 * 60
-    let maildate    = if c.req.params["timezone"].substr(0,0) == "-": dateEpoch(c.req.params["maildate"], "YYYY-MM-DD") - timezone else: dateEpoch(c.req.params["maildate"], "YYYY-MM-DD") + timezone
+  let name        = c.req.params["name"]
+  let status      = c.req.params["status"]
+  let description = c.req.params["mailtext"]
+  let timezone    = parseInt(c.req.params["timezone"].substr(1,2)) * 60 * 60
+  let maildate    = if c.req.params["timezone"].substr(0,0) == "-": dateEpoch(c.req.params["maildate"], "YYYY-MM-DD") - timezone else: dateEpoch(c.req.params["maildate"], "YYYY-MM-DD") + timezone
 
-    if maildate == 0:
-      return ("Error Mailer plugin: Maildate has a wrong format")
+  if maildate == 0:
+    return ("Error Mailer plugin: Maildate has a wrong format")
 
-    exec(db, sql"INSERT INTO mailer (name, status, description, author_id, maildate) VALUES (?, ?, ?, ?, ?)", name, status, description, c.userid, maildate)
+  exec(db, sql"INSERT INTO mailer (name, status, description, author_id, maildate) VALUES (?, ?, ?, ?, ?)", name, status, description, c.userid, maildate)
 
-    return ("OK")
+  return ("OK")
 
 
 
 proc mailerUpdateMailevent*(c: var TData, db: DbConn): string =
   ## Update a mail event
 
-  if c.loggedIn:
-    let name        = c.req.params["name"]
-    let status      = c.req.params["status"]
-    let description = c.req.params["mailtext"]
-    let timezone    = parseInt(c.req.params["timezone"].substr(1,2)) * 60 * 60
-    let maildate    = if c.req.params["timezone"].substr(0,0) == "-": dateEpoch(c.req.params["maildate"], "YYYY-MM-DD") - timezone else: dateEpoch(c.req.params["maildate"], "YYYY-MM-DD") + timezone
+  let name        = c.req.params["name"]
+  let status      = c.req.params["status"]
+  let description = c.req.params["mailtext"]
+  let timezone    = parseInt(c.req.params["timezone"].substr(1,2)) * 60 * 60
+  let maildate    = if c.req.params["timezone"].substr(0,0) == "-": dateEpoch(c.req.params["maildate"], "YYYY-MM-DD") - timezone else: dateEpoch(c.req.params["maildate"], "YYYY-MM-DD") + timezone
 
-    if maildate == 0 or maildate == 7200:
-      return ("Error Mailer plugin: Maildate has a wrong format")
+  if maildate == 0 or maildate == 7200:
+    return ("Error Mailer plugin: Maildate has a wrong format")
 
-    exec(db, sql"UPDATE mailer SET name = ?, status = ?, description = ?, author_id = ?, maildate = ? WHERE id = ?", name, status, description, c.userid, maildate, c.req.params["mailid"])
+  exec(db, sql"UPDATE mailer SET name = ?, status = ?, description = ?, author_id = ?, maildate = ? WHERE id = ?", name, status, description, c.userid, maildate, c.req.params["mailid"])
 
-    return genMailerViewMail(c, db, c.req.params["mailid"])
+  return genMailerViewMail(c, db, c.req.params["mailid"])
 
 
 proc mailerTestmail*(c: var TData, db: DbConn) =
   ## Send a test mail
 
-  if c.loggedIn:
-    let mail = getRow(db, sql"SELECT mailer.id, mailer.name, mailer.description, person.name FROM mailer LEFT JOIN person ON person.id = mailer.author_id WHERE mailer.id = ?", c.req.params["mailid"])
-    let email = getValue(db, sql"SELECT email FROM person WHERE id = ?", c.userid)
-    asyncCheck sendMailNow("Reminder: " & mail[1], mail[2], email)
+  let mail = getRow(db, sql"SELECT mailer.id, mailer.name, mailer.description, person.name FROM mailer LEFT JOIN person ON person.id = mailer.author_id WHERE mailer.id = ?", c.req.params["mailid"])
+  let email = getValue(db, sql"SELECT email FROM person WHERE id = ?", c.userid)
+  asyncCheck sendMailNow("Reminder: " & mail[1], mail[2], email)
 
 
 
 proc mailerDelete*(c: var TData, db: DbConn) =
   ## Delete a mail event
 
-  if c.loggedIn:
-    exec(db, sql"DELETE FROM mailer WHERE id = ?", c.req.params["mailid"])
+  exec(db, sql"DELETE FROM mailer WHERE id = ?", c.req.params["mailid"])
 
 
 
